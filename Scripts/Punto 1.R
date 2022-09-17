2404c1daee986a5aa5ac9d93931d76779256217b
danielraquira <df.raquira@uniandes.edu.co>
2022-09-17 18:08:25 -0500

    Se actualiza el Script del punto 1

--- Scripts/Punto 1.R
+++ Scripts/Punto 1.R
@@ -1,3 +1,13 @@
+################################################
+#                 Taller 2                     #
+#                                              #
+# Colabradores                                 #
+# Karol Gutierrez - 201816712                  #
+# Daniel Raquira - 201914059                   #
+# Santiago Becerra - 20191                     #
+################################################
+
+
 ## Punto 1 ##
 #a)
 rm(list=ls())
