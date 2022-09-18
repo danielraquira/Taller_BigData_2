d17a964b9995d4a1cca46a8023180c6cd1e4b401
SantiagoBecerra21 <santibecerraortiz@gmail.com>
2022-09-17 19:07:16 -0500

    Actualizo

--- Scripts/Punto 1.R
+++ Scripts/Punto 1.R
@@ -11,3 +11,15 @@
 ## Punto 1 ##
 #a)
 rm(list=ls())
+setwd("C:/Users/Santiago Becerra/Desktop/Santiago/Andes/Materias y Trabajos/Octavo Semestre/Big Data/Problem set 2/Taller_BigData_2/Stores")
+require(pacman)
+p_load(tidyverse, caret, rio, 
+       modelsummary, 
+       gamlr,        
+       class, skimr,dplyr, glmnet)
+library(readxl)
+train_hogares <- read_rds("train_hogares.rds")
+train_personas <- read_rds("train_hogares.rds")
+test_hogares <- read_rds("test_hogares.rds")
+test_personas <- read_rds("test_personas.rds")
+
