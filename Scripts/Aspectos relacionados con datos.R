17b4d08c7ecff845c290574ff633b06a0896267c
danielraquira <df.raquira@uniandes.edu.co>
2022-09-26 17:30:05 -0500

    Agrego script con respecto a datos

---
+++ Scripts/Aspectos relacionados con datos.R
@@ -0,0 +1,24 @@
+################################################
+#                 Taller 2                     #
+#                                              #
+# Colabradores                                 #
+# Karol Gutierrez - 201816712                  #
+# Daniel Raquira - 201914059                   #
+# Santiago Becerra - 20191                     #
+################################################
+
+
+######### Aspectos relacionados con datos ######
+#a)
+rm(list=ls())
+setwd("C:\Users\dani_\OneDrive\Escritorio\Universidad\Octavo semestre\Big data\Taller_BigData_2\Stores")
+require(pacman)
+p_load(tidyverse, caret, rio, 
+       modelsummary, 
+       gamlr,        
+       class, skimr,dplyr, glmnet)
+library(readxl)
+train_hogares <- read_rds("train_hogares.rds")
+train_personas <- read_rds("train_hogares.rds")
+test_hogares <- read_rds("test_hogares.rds")
+test_personas <- read_rds("test_personas.rds")
