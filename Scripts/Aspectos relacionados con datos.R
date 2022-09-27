7075ebd35c62c9f7ef373738d38706be90b0904c
danielraquira <df.raquira@uniandes.edu.co>
2022-09-26 22:32:14 -0500

    Se agrega base

--- Scripts/Aspectos relacionados con datos.R
+++ Scripts/Aspectos relacionados con datos.R
@@ -11,7 +11,7 @@
 ######### Aspectos relacionados con datos ######
 #a)
 rm(list=ls())
-setwd("C:/Users/Santiago Becerra/Desktop/Santiago/Andes/Materias y Trabajos/Octavo Semestre/Big Data/Problem set 2/Taller_BigData_2/Stores")
+setwd("C:/Users/dani_/OneDrive/Escritorio/Universidad/Octavo semestre/Big data/Taller_BigData_2/Stores")
 require(pacman)
 p_load(tidyverse, caret, rio, 
        modelsummary, 
@@ -19,8 +19,10 @@ p_load(tidyverse, caret, rio,
        class, skimr,dplyr, glmnet)
 library(readxl)
 train_hogares <- read_rds("train_hogares.rds")
-train_personas <- read_rds("train_hogares.rds")
+train_personas <- read_rds("train_personas.rds")
 test_hogares <- read_rds("test_hogares.rds")
 test_personas <- read_rds("test_personas.rds")
+
+
 
 
