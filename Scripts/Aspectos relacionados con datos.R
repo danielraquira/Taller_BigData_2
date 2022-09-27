832e5f6af48a146203f81b4eaca354da106fd407
danielraquira <df.raquira@uniandes.edu.co>
2022-09-26 21:59:25 -0500

    Cambio menor

--- Scripts/Aspectos relacionados con datos.R
+++ Scripts/Aspectos relacionados con datos.R
@@ -11,7 +11,7 @@
 ######### Aspectos relacionados con datos ######
 #a)
 rm(list=ls())
-setwd("C:\Users\dani_\OneDrive\Escritorio\Universidad\Octavo semestre\Big data\Taller_BigData_2\Stores")
+setwd("C:/Users/Santiago Becerra/Desktop/Santiago/Andes/Materias y Trabajos/Octavo Semestre/Big Data/Problem set 2/Taller_BigData_2/Stores")
 require(pacman)
 p_load(tidyverse, caret, rio, 
        modelsummary, 
@@ -22,3 +22,5 @@ train_hogares <- read_rds("train_hogares.rds")
 train_personas <- read_rds("train_hogares.rds")
 test_hogares <- read_rds("test_hogares.rds")
 test_personas <- read_rds("test_personas.rds")
+
+
