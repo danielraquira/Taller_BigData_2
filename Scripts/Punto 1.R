954cad952b2b9856529681742e26bea40570db33
SantiagoBecerra21 <santibecerraortiz@gmail.com>
2022-09-26 23:00:04 -0500

    Merge de sum ingresos

--- Scripts/Punto 1.R
+++ Scripts/Punto 1.R
@@ -1,7 +1,7 @@
 ################################################
 #                 Taller 2                     #
 #                                              #
-# Colabradores                                 #
+# Colaboradores                                 #
 # Karol Gutierrez - 201816712                  #
 # Daniel Raquira - 201914059                   #
 # Santiago Becerra - 201911587                 #
@@ -19,7 +19,16 @@ p_load(tidyverse, caret, rio,
        class, skimr,dplyr, glmnet)
 library(readxl)
 train_hogares <- read_rds("train_hogares.rds")
-train_personas <- read_rds("train_hogares.rds")
+train_personas <- read_rds("train_personas.rds")
 test_hogares <- read_rds("test_hogares.rds")
 test_personas <- read_rds("test_personas.rds")
 
+colnames(train_hogares)
+colnames(train_personas)
+
+## Creo la variable sum ingresos de las personas del hogar
+sum_ingresos<-train_personas %>% group_by(id) %>% summarize(Ingtot_hogar=sum(Ingtot,na.rm = TRUE)) 
+summary(sum_ingresos)
+## Merge por id para agregarle a la base de train hogares la variable de sum ingresos
+train_hogares<-left_join(train_hogares,sum_ingresos)
+colnames(train_hogares)
