4a4684f481caa0654b5a2b46bc2ba14a64c94c9d
SantiagoBecerra21 <santibecerraortiz@gmail.com>
2022-09-26 21:38:24 -0500

    Corrijo códigos

--- Scripts/Punto 1.R
+++ Scripts/Punto 1.R
@@ -4,7 +4,7 @@
 # Colabradores                                 #
 # Karol Gutierrez - 201816712                  #
 # Daniel Raquira - 201914059                   #
-# Santiago Becerra - 20191                     #
+# Santiago Becerra - 201911587                 #
 ################################################
 
 
