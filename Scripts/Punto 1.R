cddcf8d1da1a48cd244305aa829b0089673ec2bd
danielraquira <df.raquira@uniandes.edu.co>
2022-10-07 09:27:27 -0500

    Agrego los 7 modelos: 2 tradicionales, 2 lasso, 2 ridge para cada base de datos y un modelo final juntando ambas bases.

--- Scripts/Punto 1.R
+++ Scripts/Punto 1.R
@@ -141,12 +141,444 @@ train_personas <- train_personas %>%
 sum(is.na(db_trainper$Oficio))
 
 
-#Modelo 1 Tradicional Personas**************
+#Modelo 1 con train_Personas
 
-modelo1 <- lm(Ingtot_hogar ~  estrato1 +P6430 + p6040 + P6210 + p5090 + p6800 + p5130 + p5140 + Nper  , data = train_hogares)
+modelo1 <- lm(Ingtotugarr ~ P6040 + P6430 + P6210 + P6800, data = train_personas)
 summary(modelo1)
 
-#Copiar codigo!
+predicho_test<-predict(modelo1,newdata=test_personas)
+
+model1 <- train(Ingtotugarr ~ p6430 + P6040 + P6210 +P6800 ,
+                # model to fit
+                data = train_personas,
+                trControl = trainControl(method = "cv", number = 4), method = "lm")
+
+df_coeficientesm1 <- modelo1$coefficients %>%
+  enframe(name = "predictor", value = "coeficiente")
+
+df_coeficientesm1 %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes modelo tradicional") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 5, angle = 45))
+
+#Modelo 2 es hacer un ridge con las personas
+
+x_train <- model.matrix(Ingtotugarr ~ P6040 + P6430 + P6210 + P6800, data = train_personas)[, -1]
+y_train <- train_personas$Ingtotugarr
+
+#scale(x_train)
+model_2_ridge <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 0,
+  nlambda     = 100,
+  standardize = T
+)
+
+regularizacion <- model_2_ridge$beta %>% 
+  as.matrix() %>%
+  t() %>% 
+  as_tibble() %>%
+  mutate(lambda = model_2_ridge$lambda)
+
+regularizacion <- regularizacion %>%
+  pivot_longer(
+    cols = !lambda, 
+    names_to = "predictor",
+    values_to = "coeficientes"
+  )
+
+regularizacion %>%
+  ggplot(aes(x = lambda, y = coeficientes, color = predictor)) +
+  geom_line() +
+  scale_x_log10(
+    breaks = trans_breaks("log10", function(x) 10^x),
+    labels = trans_format("log10", math_format(10^.x))
+  ) +
+  labs(title = "Coeficientes en función de la regularización para m2") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+cv_error <- cv.glmnet(
+  x      = x_train,
+  y      = y_train,
+  alpha  = 0,
+  nfolds = 10,
+  type.measure = "mse",
+  standardize  = T
+)
+
+plot(cv_error)
+paste("Mejor valor de lambda:", cv_error$lambda.min)
+paste("Mejor valor de lambda + 1 desviación estandar:", cv_error$lambda.1se)
+
+modelo2 <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 0,
+  lambda      = cv_error$lambda.min,
+  standardize = TRUE
+)
+
+df_coeficientesm2 <- coef(modelo2) %>%
+  as.matrix() %>%
+  as_tibble(rownames = "predictor") %>%
+  rename(coeficiente = s0)
+
+df_coeficientes %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes modelo Ridge") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 6, angle = 45))
+
+#Modelo 3 con lasso y Personas
+
+x_train <- model.matrix(~ P6040 + P6430 + P6210 + P6800, data = train_personas)
+y_train <- db_trainper$Ingtot
+
+x_test<- model.matrix(~ P6040 + P6430 + P6210 + P6800, data = test_personas)
+
+
+modelo_3_lasso <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  nlambda     = 100,
+  standardize = TRUE
+)
+
+regularizacion <- modelo_3_lasso$beta %>% 
+  as.matrix() %>%
+  t() %>% 
+  as_tibble() %>%
+  mutate(lambda = modelo_3_lasso$lambda)
+
+regularizacion <- regularizacion %>%
+  pivot_longer(
+    cols = !lambda, 
+    names_to = "predictor",
+    values_to = "coeficientes"
+  )
+
+regularizacion %>%
+  ggplot(aes(x = lambda, y = coeficientes, color = predictor)) +
+  geom_line() +
+  scale_x_log10(
+    breaks = trans_breaks("log10", function(x) 10^x),
+    labels = trans_format("log10", math_format(10^.x))
+  ) +
+  labs(title = "Coeficientes modelo regularizaci?n") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+
+cv_error <- cv.glmnet(
+  x      = x_train,
+  y      = y_train,
+  alpha  = 1,
+  nfolds = 10,
+  type.measure = "mse",
+  standardize  = TRUE
+)
+
+plot(cv_error)
+paste("Mejor valor de lambda:", cv_error$lambda.min)
+paste("Mejor valor de lambda + 1 desviación estandar:", cv_error$lambda.1se)
+
+
+modelo3 <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  lambda      = cv_error$lambda.min,
+  standardize = TRUE
+)
+
+df_coeficientesm3 <- coef(modelo3) %>%
+  as.matrix() %>%
+  as_tibble(rownames = "predictor") %>%
+  rename(coeficiente = s0)
+
+df_coeficientes %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes modelo Lasso") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 6, angle = 45))
+
+ing_predicho<-data.frame()
+ing_predicho<-predict(modelo3,newx=xtest)
+ingtot_pred<- cbind(ing_predicho, test_personas$id)
+ingtot_pred<-as.data.frame(ingtot_pred)
+ingtot_pred$s0 <- as.numeric(ingtot_pred$s0)
+aggregate<-aggregate(s0 ~ V2, data=ingtot_pred, FUN=sum)
+
+aggregate<-rename(aggregate, ingtot = s0)
+aggregate<-rename(aggregate, id = V2)
+
+
+##Nuevo merge
+test_hogares2<-as.data.frame(test_hogares)
+Final <- merge(aggregate,test_hogares2,by="id")
+Final <- Final %>% mutate(ingtotper=ingtot/Npersug)
+Final <- Final[c("id","ingtot","ingtotper","Npersug","Lp")]
+total$Pobre_ingtot<-ifelse(total$ingtotper<total$Lp,1,0)
+
+#Modelo 4  tradicional Hogares
+
+modelo4 <- lm(Ingtotugarr  ~ p5090 + Nper + p5000 + Arri, data = train_hogares)
+summary(modelo4)
+
+model4 <- train(Ingtotugarr  ~ p5090 + Nper + p5000 + Arri
+                # model to fit
+                data = train_hogares,
+                trControl = trainControl(method = "cv", number = 5), method = "lm")
+
+
+df_coeficientesm4 <- model4$coefficients %>%
+  enframe(name = "predictor", value = "coeficiente")
+
+df_coeficientesm4 %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes modelo tradicional") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 5, angle = 45))
+
+#Modelo 5 hacer ridge con hogares
+
+x_train <- model.matrix(Ingtotugarr  ~ p5090 + Nper + p5000 + Arri, data = train_hogares)[, -1]
+y_train <- train_hogares$Ingtotugarr
+
+#scale(x_train)
+model_5_ridge <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 0,
+  nlambda     = 100,
+  standardize = T
+)
+
+regularizacion <- model_5_ridge$beta %>% 
+  as.matrix() %>%
+  t() %>% 
+  as_tibble() %>%
+  mutate(lambda = model_5_ridge$lambda)
+
+regularizacion <- regularizacion %>%
+  pivot_longer(
+    cols = !lambda, 
+    names_to = "predictor",
+    values_to = "coeficientes"
+  )
+
+regularizacion %>%
+  ggplot(aes(x = lambda, y = coeficientes, color = predictor)) +
+  geom_line() +
+  scale_x_log10(
+    breaks = trans_breaks("log10", function(x) 10^x),
+    labels = trans_format("log10", math_format(10^.x))
+  ) +
+  labs(title = "Coeficientes modelo según regularización") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+cv_error <- cv.glmnet(
+  x      = x_train,
+  y      = y_train,
+  alpha  = 0,
+  nfolds = 10,
+  type.measure = "mse",
+  standardize  = T
+)
+
+plot(cv_error)
+paste("Mejor valor de lambda:", cv_error$lambda.min)
+paste("Mejor valor de lambda + 1 desviación estandar:", cv_error$lambda.1se)
+
+modelo5 <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 0,
+  lambda      = cv_error$lambda.1se,
+  standardize = TRUE
+)
+
+df_coeficientesm5 <- coef(modelo5) %>%
+  as.matrix() %>%
+  as_tibble(rownames = "predictor") %>%
+  rename(coeficiente = s0)
+
+df_coeficientesm5 %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes del modelo Ridge") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 6, angle = 45))
+
+#Modelo 6 hacer un lasso con Hogares****************************************
+
+x_train <- model.matrix(Ingtotugarr  ~ p5090 + Nper + p5000 + Arri, data = train_hogares)[, -1]
+y_train <- train_hogares$Ingtotugarr
+
+modelo_6_lasso <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  nlambda     = 100,
+  standardize = TRUE
+)
+
+regularizacion <- modelo_6_lasso$beta %>% 
+  as.matrix() %>%
+  t() %>% 
+  as_tibble() %>%
+  mutate(lambda = modelo_6_lasso$lambda)
+
+regularizacion <- regularizacion %>%
+  pivot_longer(
+    cols = !lambda, 
+    names_to = "predictor",
+    values_to = "coeficientes"
+  )
+
+regularizacion %>%
+  ggplot(aes(x = lambda, y = coeficientes, color = predictor)) +
+  geom_line() +
+  scale_x_log10(
+    breaks = trans_breaks("log10", function(x) 10^x),
+    labels = trans_format("log10", math_format(10^.x))
+  ) +
+  labs(title = "Coeficientes del modelo en funci?n de la regularizaci?n") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+
+cv_error <- cv.glmnet(
+  x      = x_train,
+  y      = y_train,
+  alpha  = 1,
+  nfolds = 10,
+  type.measure = "mse",
+  standardize  = TRUE
+)
+
+plot(cv_error)
+paste("Mejor valor de lambda:", cv_error$lambda.min)
+paste("Mejor valor de lambda + 1 desviación estandar:", cv_error$lambda.1se)
+
+
+modelo6 <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  lambda      = cv_error$lambda.1se,
+  standardize = TRUE
+)
+
+df_coeficientesm6 <- coef(modelo6) %>%
+  as.matrix() %>%
+  as_tibble(rownames = "predictor") %>%
+  rename(coeficiente = s0)
+
+df_coeficientesm6 %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes del modelo Lasso") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 6, angle = 45))
+
+#Modelo 7: Se realiza una mezcla entre base de personas y de hogares para escoger el mejor modelo posible 
+## Esto toca analizarlo mayor profundidad cuando corramos los 6 modelos anteriores.
+##Ultimo merge
+train_general <- merge(train_hogares,train_personas,by="id")
+test_general <- merge(test_hogares,test_personas,by="id")
+
+##Comienzo del modelo
+x_train <- model.matrix(~ p6430 + p6040 + p6210 + p5090 + Nper + p6800 + Arri + p5000, train_general)
+y_train <- train_general$Ingtotugarr
+x_test<- model.matrix(~ p6430 + p6040 + p6210 + p5090 + Nper + p6800 + Arri + p5000, test_general)
+
+modelo_7_lasso <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  nlambda     = 100,
+  standardize = TRUE
+)
+
+
+regularizacion <- modelo_7_lasso$beta %>% 
+  as.matrix() %>%
+  t() %>% 
+  as_tibble() %>%
+  mutate(lambda = modelo_7_lasso$lambda)
+
+regularizacion <- regularizacion %>%
+  pivot_longer(
+    cols = !lambda, 
+    names_to = "predictor",
+    values_to = "coeficientes"
+  )
+
+regularizacion %>%
+  ggplot(aes(x = lambda, y = coeficientes, color = predictor)) +
+  geom_line() +
+  scale_x_log10(
+    breaks = trans_breaks("log10", function(x) 10^x),
+    labels = trans_format("log10", math_format(10^.x))
+  ) +
+  labs(title = "Coeficientes modelo según regularización") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+
+cv_error <- cv.glmnet(
+  x      = x_train,
+  y      = y_train,
+  alpha  = 1,
+  nfolds = 10,
+  type.measure = "mse",
+  standardize  = TRUE
+)
+
+plot(cv_error)
+paste("Mejor valor de lambda:", cv_error$lambda.min)
+paste("Mejor valor de lambda + 1 desviaci?n est?ndar:", cv_error$lambda.1se)
+
+
+modelo7 <- glmnet(
+  x           = x_train,
+  y           = y_train,
+  alpha       = 1,
+  lambda      = cv_error$lambda.min,
+  standardize = TRUE
+)
+
+
+# Coeficientes del modelo
+df_coeficientesm7 <- coef(modelo7) %>%
+  as.matrix() %>%
+  as_tibble(rownames = "predictor") %>%
+  rename(coeficiente = s0)
+
+df_coeficientesm7 %>%
+  filter(predictor != "(Intercept)") %>%
+  ggplot(aes(x = predictor, y = coeficiente)) +
+  geom_col() +
+  labs(title = "Coeficientes del modelo Lasso") +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 6, angle = 45))
+
 
        
 
